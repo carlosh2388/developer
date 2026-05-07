e6b25907902e232a93736f9fbd60b0f9a12d4a6a
Neldy Velásquez <neldy.velasquez@gmail.com>
2026-05-07 09:22:43 -0600

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+// Importar librerías
+const express = require("express");
+const cors = require("cors");
+
+const usuariosRoutes = require("./routes/usuarios");
+
+// Crear app
+const app = express();
+
+// Permite conexión desde frontend
+app.use(cors());
+
+// Permite trabajar con JSON
+app.use(express.json());
+
+
+/* =========================
+   RUTA DE USUARIOS
+========================= */
+
+// Rutas API
+app.use("/api/usuarios", usuariosRoutes);
+
+// Ruta base
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+/* =========================
+   INICIAR SERVIDOR
+========================= */
+// Puerto
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
