a364317006bcbc3968ec9cd600bcf8625161103d
Neldy Velásquez <neldy.velasquez@gmail.com>
2026-06-01 16:01:17 -0600

    Update IngresoHuevos.js

--- frontend/src/pages/IngresoHuevos.js
+++ frontend/src/pages/IngresoHuevos.js
@@ -316,11 +316,31 @@ function IngresoHuevos() {
             <div>
               <label>Total Unidades</label>
 
-              <input
-                type="number"
-                value={totalGrupo}
-                readOnly
-              />
+            <div
+  style={{
+    display: "flex",
+    flexDirection: "column"
+  }}
+>
+
+  <label>Total</label>
+
+  <div
+    style={{
+      padding: "6px 10px",
+      border: "1px solid #ccc",
+      borderRadius: "4px",
+      minWidth: "80px",
+      textAlign: "center",
+      background: "#f8f8f8",
+      fontWeight: "500"
+    }}
+  >
+    {totalGrupo}
+  </div>
+
+</div>
+    
             </div>
 
           </div>
